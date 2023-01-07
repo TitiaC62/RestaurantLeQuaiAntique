bfdc92444178c37cf71750ee56fb0e074c7cff42
TitiaC62 <laetitia.crombet@gmail.com>
2023-01-07 18:00:11 +0100

    add header and footer php

---
+++ index.php
@@ -0,0 +1,69 @@
+<?php require_once 'layout/header.php' ?>
+<?php require_once 'layout/nav.php' ?>
+
+    <main>
+        <div>
+            <div id="SlideAccueil" class="carousel slide" data-bs-ride="carousel">
+                <div class="carousel-inner ">
+                <div class="carousel-item active ">
+                    <img src="/Media/jason-leung-poI7DelFiVA-unsplash.jpg" class=" slide-accueil d-block mx-auto " alt="salle du restaurant">
+                </div>
+                <div class="carousel-item">
+                    <img src="/Media/hd-wallpaper-g5d20ceffd_640.jpg" class=" slide-accueil d-block mx-auto" alt="terasse extérieure">
+                </div>
+                <div class="carousel-item">
+                    <img src="/Media/tapas-gcf82ec276_640.jpg" class=" slide-accueil d-block mx-auto" alt="plat de charcuterie">
+                </div>
+                </div>
+            </div>
+        </div>
+
+        <div>
+            <div class="container text-center ">
+                <div class="row  row-cols-lg-6 g-1 g-lg-2 m-5">
+                    <div class="col">
+                        <div class="p-2">
+                            <img src="/Media/hors-doeuvre-2175326__340.jpg" alt="entrée" class="img-line">
+                        </div>
+                    </div>
+                    <div class="col">
+                        <div class="p-2">
+                            <img src="/Media/asparagus-gaa5bc2538_640.jpg" alt="" class="img-line">
+                        </div>
+                    </div>
+                    <div class="col">
+                        <div class="p-2">
+                            <img src="/Media/salad-g319afc9fb_640.jpg" alt="" class="img-line">
+                        </div>
+                    </div>
+                    <div class="col">
+                        <div class="p-2">
+                            <img src="/Media/raclette-ge3ad1e454_640.jpg" alt="" class="img-line">
+                        </div>
+                    </div>
+                    <div class="col">
+                        <div class="p-2">
+                            <img src="/Media/eclair-g41f2b224f_640.jpg" alt="" class="img-line" >
+                        </div>
+                    </div>
+                    <div class="col">
+                    <div class="p-2">
+                        <img src="/Media/jonathan-borba-uB7q7aipU2o-unsplash.jpg" alt="" class="img-line">
+                    </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div>
+            <div class="mb-5">
+                <p class="reservation mx-auto text-center py-3">Réservation</p>
+            </div>
+        </div>
+
+    </main>
+
+
+<!--FOOTER-->
+
+<?php require_once 'layout/footer.php' ?>
