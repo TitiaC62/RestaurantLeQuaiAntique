c8553756e6169cbfc8fc09e64021ebc4077613c7
TitiaC62 <laetitia.crombet@gmail.com>
2023-03-04 23:04:22 +0100

    add starter and main - carte

--- pages/carte.php
+++ pages/carte.php
@@ -2,11 +2,187 @@
 <?php require_once '../layout/nav.php' ?>
 
 <section>
-    <h1> Carte</h1>
+    <h1  class="titrecarte"> Carte</h1>
 
     <h2 class="titreh2">Entrées</h2>
+
+        <div >
+               <ul>
+                   <li class="title">
+                       <span>Salade verte </span>             
+                       <span class="price">2.90€ </span>
+                   </li>
+                   <li class="title">
+                       <span>Soupe maison </span>             
+                       <span class="price">5.90€ </span>
+                   </li>
+                   <li class="title">
+                       <span>Salade verte + tomates + oeufs + olives + vinaigrette </span>             
+                       <span class="price">7.20€ </span>
+                   </li>
+                   <li class="title">
+                       <span>Salade de chèvre chaud sur toast au miel XXL </span>             
+                       <span class="price">12.50€ </span>
+                   </li>  
+                   <li class="paragrapheliste">
+                       <p class="liste ingredients">Salade, toast, tomates, ciboulette, miel, chèvre, noix, morceaux de pomme, sauce vinaigrette </p> 
+                   </li>    
+                   
+                   <li class="title">
+                       <span>Salade de gésiers de volaille XXL</span>
+                       <span class="price"> 14.50€</span>
+                   </li> 
+                   <li class="paragrapheliste">
+                         <p class="liste ingredients"> Salade verte, gésiers, lardons, tomates, oeufs, cubes de pommes de terre, croutons, vinaigre de framboise</p> 
+                   </li>    
+                  
+                   <li class="title">
+                       <span>Camembert rôti et sa charcuterie XXL </span>
+                       <span class="price">16.50€ </span>
+                   </li>
+                   <li class="paragrapheliste">
+                    <p class="liste ingredients">Fromage camembert, saucisson sec, saucisson à l’ail, rosette, </br> jambon fumé, jambon blanc, cornichons, beurre, oignon rouge, rondelles de pommes de terre, sauce vinaigrette </p> 
+                   </li>
+               </ul>             
+           </div>
+
     <h2 class="titreh2">Plats</h2>
+
+    <ul>    <h3 class="titreh3"> Les viandes </h3>
+                       <li class="title">
+                           <span>Steak haché façon bouchère origine France 180 g </span>
+                           <span class="price">11.80€ </span>
+                       </li>
+                       <li class="title">
+                           <span>Pavé du boucher origine UE 180g </span>
+                           <span class="price">18.50€ </span>
+                       </li>
+                       <li class="title">
+                           <span>Brochette de boeuf </span>
+                           <span class="price">17.50€ </span>
+                       </li>
+
+
+            <h3 class="titreh3"> Les spécialités savoyardes </h3>
+                      
+                       <li class="title">
+                           <span>Tartiflette </span>
+                           <span class="price">15.80€ </span>
+                       </li>
+                       <li class="paragrapheliste">
+                       <p class="liste ingredients">Pommes de terre, lardons, Reblochon fondu, oignons, crème, charcuterie et salade</p>
+                        </li>
+                       
+                       <li class="title">
+                           <span>Croziflette</span>
+                           <span class="price"> 15.80€ </span>
+                       </li>
+                       <li class="paragrapheliste">
+                       <p class="liste ingredients ">Pâtes aux Crozets, jambon de pays de Savoie, oignons, Reblochon fondu, lardons, crème, charcuterie et salade</p>
+                        </li>
+
+                        <li class="title">
+                           <span>Raclette (2 personnes mini)</span>
+                           <span class="price"> 21€ / personne </span>
+                       </li>
+                       <li class="paragrapheliste">
+                       <p class="liste ingredients ">Fromage, charcuterie, pomme de terre</p>
+                        </li>
+
+                        <li class="title">
+                           <span>Fondue soyarde (2 personnes mini) </span>
+                           <span class="price">19.80€ </span>
+                       </li>
+                       <li class="paragrapheliste">
+                       <p class="liste ingredients ">Fromage (Emmental, Comté, Beaufort), charcuterie, pomme de terre, salade</p>
+                        </li>
+                    
+                    
+
+            <h3 class="titreh3"> La brasserie </h3>
+
+                        <li class="title">
+                           <span>Oeuf sur le plat</span>
+                           <span class="price"> 10.80€ </span>
+                       </li>
+                       <li class="title">
+                           <span>Hamburger frites maison</span>
+                           <span class="price"> 15.80€ </span>
+                       </li>
+                       
+                   </ul>
+
     <h2 class="titreh2">Desserts</h2>
+
+        
+    <div >
+       
+                   <ul>
+                       <li class="title">
+                           <span>Ile Flottante </span>
+                           <span class="price">6.80€ </span>
+                       </li>
+                       <li class="title">
+                           <span>Crème brûlée </span>
+                           <span class="price">6.80€ </span>
+                       </li>
+                       <li class="title">
+                           <span>Mousse au chocolat </span>
+                           <span class="price">4.20€ </span>
+                       </li>
+                       <li class="title">
+                           <span>Tarte tatin et sa boule de glace vanille </span>
+                           <span class="price">7.50€ </span>
+                       </li>
+                       <li class="title">
+                           <span>Café / thé gourmand </span>
+                           <span class="price">8.50€ </span>
+                       </li>
+                     
+                       <li class="title">
+                            <span>Coupe 1 boule </span>
+                            <span class="price">2.50€ </span>
+                        </li>
+                        <li class="title">
+                            <span>Coupe 2 boules </span>
+                            <span class="price">3.90€ </span>
+                        </li>
+                        <li class="title">
+                            <span>Coupe 3 boules </span>
+                            <span class="price">5.80€ </span>
+                        </li>
+                        <li class="paragrapheliste">
+                           <p class="liste ingredients">Parfums au choix : chocolat, vanille, fraise, café, menthe chocolat, citron, cerise, coco,caramel, framboise </p> 
+                        </li>
+       
+                        <li class="title">
+                            <span>Café Liégeois </span>
+                            <span class="price">7.50€ </span>
+                        </li>
+                        <li class="paragrapheliste">
+                           <p class="liste ingredients">Glace café 2 boules, glace vanille 1 boule, coulis café et chantilly </p>   
+                        </li>
+       
+                        <li class="title">
+                            <span> Chocolat Liégeois </span>
+                            <span class="price">7.50€ </span>
+                        </li>
+                        <li class="paragrapheliste">
+                            <p class="liste ingredients"> Glace chocolat 2 boules, glace vanille 1 boule, coulis chocolat et chantilly </p>  
+                        </li>
+       
+       
+                        <li class="title">
+                            <span>La Dame Blanche </span>
+                            <span class="price">7.50€ </span>
+                        </li>
+                        <li class="paragrapheliste">
+                            <p class="liste ingredients">Glace vanille 3 boules, coulis chocolat et chantilly </p>   
+                        </li>
+                
+                  </ul>
+       
+         </div>
 
     <h2 class="titreh2">Boissons </h2>
 
--- pages/menus.php
+++ pages/menus.php
@@ -2,6 +2,95 @@
 <?php require_once '../layout/nav.php' ?>
 
 
+<h1 class="titrecarte"> Menus</h1>
+   
+
+<h2 class="titreh2"> Menu Piste Verte - 9,90€</h2>
+
+        <p>Jusqu'à 12 ans</p>
+
+        <ul>
+            <li>Steack haché façon bouchère</li>
+            <li>Nuggets maison</li>
+            <li>Oeuf sur le plat</li>
+            <li>Mini burger frites</li>
+        </ul>
+
+        <ul>
+            <li>Glace 1 boule</li>
+            <li>Crêpe au sucre ou chocolat</li>
+            <li>Mini Mousse au chocolat</li>
+        </ul>
+        
+<h2 class="titreh2"> Menu Piste Bleue (express)</h2>
+
+<p> Du mardi au samedi midi, à consulter sur nos tableaux, selon disponibilité </p>
+        <p>Entrée + Plat + Café ou Plat + Dessert + Café : 13€ </p> 
+        <p>Entrée + Plat + Dessert + Café : 16€</p>
+
+        <ul>
+            <li>Salade verte</li>
+            <li>Soupe maison</li>
+            <li>Petite assiette de charcuterie</li>
+        </ul>
+
+        <ul>
+            <li>Steack haché façon bouchère</li>
+            <li>hamburger maison</li>
+            <li>Oeuf sur le plat</li>
+        </ul>
+
+        <ul>
+            <li>Glace 1 boule</li>
+            <li>Crêpe au sucre</li>
+            <li>Mousse au chocolat</li>
+        </ul>
+
+<h2 class="titreh2"> Menu Piste Rouge - 28,00€</h2>
+
+      
+        <ul>
+            <li >Entrée au choix </li>
+        </ul>
+
+        <ul>
+            <li>Spécialité ou plat aux choix</li>
+        </ul>
+
+        <ul>
+            <li >Dessert aux choix</li>
+        </ul>
+
+
+<h2 class="titreh2"> Menu Piste Noire - 38,00€ </h2>
+
+        <ul>
+            <li > Cocktail au choix</li>
+            <li> Kir savoyard (12cl)</li>
+        </ul>
+
+        <ul>
+            <li >Entrée au choix </li>
+        </ul>
+
+        <ul>
+            <li>Spécialité ou plat aux choix</li>
+        </ul>
+
+        <ul>
+            <li >Dessert aux choix</li>
+        </ul>
+
+        <ul>
+            <li>Boisson</li>
+            <li>1 verre de vin de Savoie</li>
+            <li>ou 1 bière 25cl</li>
+            <li>ou 1 soft 25cl</li>
+            <li>ou 1 bouteille d'eau 50cl</li>
+        </ul>
+
+
+
 
 <!--FOOTER-->
 
